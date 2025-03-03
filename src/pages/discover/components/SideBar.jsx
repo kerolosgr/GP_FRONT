c91febb8aa82dba4d136b46034e913b774b6e039
kerolosgr <kerolossafwat44@gmail.com>
2025-03-03 18:23:58 +0200

    profile part

--- src/pages/discover/components/SideBar.jsx
+++ src/pages/discover/components/SideBar.jsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { BriefcaseBusiness, Earth, Mail, Scroll, Video } from "lucide-react";
 import Tooltipcustom from "./Tooltipcustom";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Badge } from "@/components/ui/badge";
 
 
 const SideBar = ()=>{
@@ -34,23 +35,39 @@ const SideBar = ()=>{
             </div>
             <div className="w-[calc(100%-70px)] p-4">
 
-                <div className="flex flex-col justify-start items-center w-[500px] h-full bg-neutral-50 rounded-xl p-2 shadow-xl">
-                    <div className="w-full flex justify-start items-center h-[40%]">
+                <div className="flex flex-col justify-start items-center w-[500px] h-fit bg-neutral-50 rounded-xl px-2 py-[50px] shadow-xl">
+                    <div className="w-full flex justify-center items-center h-[40%]">
                     <Avatar className="w-[200px] drop-shadow-xl">
                     <AvatarImage className="rounded-full" src="https://avatars.githubusercontent.com/u/183719833?v=4" />
                     <AvatarFallback>O</AvatarFallback>
                     </Avatar>
                     <div>
                     <h2 className="ml-4 text-3xl font-bold font-sans">Kerolos Safwat</h2>
-                    <span className="ml-4 text-sm font-medium font-sans">
-                    Software Engineer
-                    </span>
+                    <span className="ml-4 text-sm font-medium font-sans">Software Engineer</span>
                     </div>
                     </div>
 
-                    <div className="w-full grid grid-cols-2 gap-x-5 gap-y-3">
+                    <div className="w-full grid grid-cols-2 gap-x-5 gap-y-3 mt-4">
                         <span className="col-span-1 flex"><Mail className="mr-2" /><p>Kerolossafwat41@gmail.com</p></span>
                         <span className="col-span-1 flex"><Earth className="mr-2" /><p>Egypt</p></span>
+                    </div>
+
+                    <div className="w-full flex flex-col justify-center items-start mt-4">
+                        <h3 className="text-2xl ml-2 font-semibold">Skills</h3>
+                        <div className="w-full flex m-2 flex-wrap">
+                        <Badge variant="dark">Javascript</Badge>
+                        <Badge variant="dark">HTML</Badge>
+                        <Badge variant="dark">CSS</Badge>
+                        <Badge variant="dark">ReactJS</Badge>
+                        <Badge variant="dark">NextJS</Badge>
+                        <Badge variant="dark">MONGO</Badge>
+                        <Badge variant="dark">SQL</Badge>
+                        <Badge variant="dark">NODEJS</Badge>
+
+
+                        </div>
+
+
                     </div>
 
                 </div>
