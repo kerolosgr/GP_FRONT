e354ae3d0169c41bbf7b3fef9d2af2a712f2d17d
kerolosgr <kerolossafwat44@gmail.com>
2025-03-04 18:16:38 +0200

    job dialog and edit in skeleton

--- src/pages/discover/components/JobCard.jsx
+++ src/pages/discover/components/JobCard.jsx
@@ -1,25 +1,61 @@
-import { Activity } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Activity, Stars } from "lucide-react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const JobCard = ()=>{
+const JobCard = () => {
+    const [isVisible,setisVisible] = useState(false);
     return (
-        <div className="w-[420px] h-[300px] p-4 flex flex-col justify-start items-start border rounded-xl border-gray-300 hover:shadow transition">
-                    <div className="w-full flex justify-between items-center">
-                        <div>
-                            <h5 className="font-semibold text-[18px] my-2">Software Engineer</h5>
-                            <p>Advansys</p>
-                            <p>Nasr City</p>
-                            <span className="bg-[#e8f3fc] min-w-[120px] max-w-[200px] h-[25px] flex justify-start py-2 px-4 items-center text-center rounded text-[12px] font-semibold my-2"><Activity size={15} color="#237ea3" className="mr-2" />3 days ago</span>
-                        </div>
-                        <div className="w-[110px] h-[110px]">
-                            <img className="object-contain" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1024px-Google_%22G%22_logo.svg.png" width={110} height={110} alt="co. img" />
-                        </div>
+        <>
+            <div onClick={() => setisVisible(true)} className="w-[420px] h-[300px] p-4 flex flex-col justify-start items-start border rounded-xl border-gray-300 hover:shadow transition">
+                <div className="w-full flex justify-between items-center">
+                    <div>
+                        <h5 className="font-semibold text-[18px] my-2">Software Engineer</h5>
+                        <p>Advansys</p>
+                        <p>Nasr City</p>
+                        <span className="bg-[#e8f3fc] min-w-[120px] max-w-[200px] h-[25px] flex justify-start py-2 px-4 items-center text-center rounded text-[12px] font-semibold my-2"><Activity size={15} color="#237ea3" className="mr-2" />3 days ago</span>
                     </div>
-                    <div className="flex justify-start items-center gap-2 text-[14px]">
-                        <p>Full-time</p>
-                        <p>Remote</p>
+                    <div className="w-[110px] h-[110px]">
+                        <img className="object-contain" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1024px-Google_%22G%22_logo.svg.png" width={110} height={110} alt="co. img" />
                     </div>
-                    <p className="mt-2 text-[12px] font-semibold text-gray-700 line-clamp-5">Join Advansys as a Software Engineer Intern and gain hands-on experience in a fast-paced and innovative environment. You'll work alongside experienced engineers and contribute to various software development projects, enhancing your technical skills while learning about real-world applications.Join Advansys as a Software Engineer Intern and gain hands-on experience in a fast-paced and innovative environment. You'll work alongside experienced engineers and contribute to various software development projects, enhancing your technical skills while learning about real-world applications.</p>
                 </div>
+                <div className="flex justify-start items-center gap-2 text-[14px]">
+                    <p className="font-semibold">Entry-level</p>
+                    <p>Full-time</p>
+                    <p>Remote</p>
+                </div>
+                <p className="mt-2 text-[12px] font-semibold text-gray-700 line-clamp-5">Join Advansys as a Software Engineer Intern and gain hands-on experience in a fast-paced and innovative environment. You'll work alongside experienced engineers and contribute to various software development projects, enhancing your technical skills while learning about real-world applications.Join Advansys as a Software Engineer Intern and gain hands-on experience in a fast-paced and innovative environment. You'll work alongside experienced engineers and contribute to various software development projects, enhancing your technical skills while learning about real-world applications.</p>
+            </div>
+            <Dialog open={isVisible} onOpenChange={setisVisible}>
+            <DialogContent className="min-w-[1020px] min-h-[50vh] max-h-[80vh]">
+                <div className="flex flex-col w-full h-full items-start justify-start p-[20px]">
+                <div className="w-full flex justify-between items-center">
+                    <div>
+                        <h5 className="font-semibold text-[18px] my-2">Software Engineer</h5>
+                        <p>Advansys</p>
+                        <p>Nasr City</p>
+                        <span className="bg-[#e8f3fc] min-w-[120px] max-w-[200px] h-[25px] flex justify-start py-2 px-4 items-center text-center rounded text-[12px] font-semibold my-2"><Activity size={15} color="#237ea3" className="mr-2" />3 days ago</span>
+                    </div>
+                    <div className="w-[110px] h-[110px]">
+                        <img className="object-contain" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1024px-Google_%22G%22_logo.svg.png" width={110} height={110} alt="co. img" />
+                    </div>
+                </div>
+                <div className="flex justify-start items-center gap-2 text-[14px]">
+                    <p className="font-semibold">Entry-level</p>
+                    <p className="font-semibold">Full-time</p>
+                    <p className="font-semibold">Remote</p>
+                </div>
+                <p className="mt-2 text-[14px] font-semibold text-gray-700">Join Advansys as a Software Engineer Intern and gain hands-on experience in a fast-paced and innovative environment. You'll work alongside experienced engineers and contribute to various software development projects, enhancing your technical skills while learning about real-world applications.Join Advansys as a Software Engineer Intern and gain hands-on experience in a fast-paced and innovative environment. You'll work alongside experienced engineers and contribute to various software development projects, enhancing your technical skills while learning about real-world applications.</p>
+                <div className="mt-10 ml-auto flex justify-center items-center">
+                <Link className="bg-black font-semibold text-white rounded text-[14px] py-2 px-10 hover:bg-neutral-900">Apply</Link>
+                <Button className="mx-2 rounded" variant={"dark"}><Stars  />Mark As Favorite</Button>
+                </div>
+                
+                </div>
+            </DialogContent>
+        </Dialog >
+        </>
     )
 }
 export default JobCard;
--- src/pages/discover/components/JobCardSkeleton.jsx
+++ src/pages/discover/components/JobCardSkeleton.jsx
@@ -1,5 +1,4 @@
 import { Skeleton } from "@/components/ui/skeleton";
-import { Activity } from "lucide-react";
 
 
 const JobCardSkeleton = ()=>{
@@ -7,16 +6,17 @@ const JobCardSkeleton = ()=>{
         <div className="w-[420px] h-[300px] p-4 flex flex-col justify-start items-start border rounded-xl border-gray-300 hover:shadow transition">
                     <div className="w-full flex justify-between items-center">
                         <div>
-                            <Skeleton className="w-full h-[20px] rounded-lg my-2"/>
-                            <Skeleton className="w-full h-[10px] rounded-lg mb-1"/>
-                            <Skeleton className="w-[40px] h-[10px] rounded-lg"/>
+                            <Skeleton className="w-full h-[25px] rounded-lg my-4"/>
+                            <Skeleton className="w-full h-[15px] rounded-lg mb-1"/>
+                            <Skeleton className="w-[40px] h-[15px] rounded-lg"/>
                             <Skeleton className="w-[140px] my-2 h-[25px] rounded-lg"/>
                         </div>
                         <Skeleton className="w-[110px] h-[110px] rounded-lg"/>
                     </div>
-                    <div className="flex justify-start items-center gap-2 text-[14px]">
-                    <Skeleton className="w-[70px] h-[10px] rounded-lg mb-1"/>
-                    <Skeleton className="w-[70px] h-[10px] rounded-lg mb-1"/>
+                    <div className="flex justify-start items-center my-2 gap-2 text-[14px]">
+                    <Skeleton className="w-[50px] h-[15px] rounded-lg mb-1"/>
+                    <Skeleton className="w-[50px] h-[15px] rounded-lg mb-1"/>
+                    <Skeleton className="w-[50px] h-[15px] rounded-lg mb-1"/>
                     </div>
                     <Skeleton className="w-full h-[10px] rounded-lg mb-1"/>
                     <Skeleton className="w-full h-[10px] rounded-lg mb-1"/>
