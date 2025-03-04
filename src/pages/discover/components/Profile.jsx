9bcc058fb4af2e7219d167a9ae15237043dfa265
kerolosgr <kerolossafwat44@gmail.com>
2025-03-04 14:47:40 +0200

    profile and sidebar

---
+++ src/pages/discover/components/Profile.jsx
@@ -0,0 +1,49 @@
+import { Badge } from "@/components/ui/badge";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Book, Earth, IdCard, Mail } from "lucide-react";
+const Profile = ()=>{
+    return(
+        <>
+        <div className="flex flex-col justify-start items-center w-[500px] h-fit bg-neutral-50 rounded-xl px-2 py-[25px] shadow-xl">
+                    <div className="w-full flex justify-center items-center h-[40%]">
+                    <Avatar className="w-[200px] drop-shadow-xl">
+                    <AvatarImage className="rounded-full shadow-lg" src="https://avatars.githubusercontent.com/u/183719833?v=4" />
+                    <AvatarFallback><img className="rounded-full shadow-lg" src="/assets/user.webp" /></AvatarFallback>
+                    </Avatar>
+                    <div>
+                    <h2 className="ml-4 text-3xl font-bold font-sans">Kerolos Safwat</h2>
+                    <span className="ml-4 text-sm font-medium font-sans">Software Engineer</span>
+                    </div>
+                    </div>
+
+                    <div className="w-full flex flex-col p-2 gap-y-3 mt-4">
+                    <span className="flex"><IdCard className="mr-2" /><p className="font-bold">532016</p></span>
+                        <span className="flex"><Mail className="mr-2" /><p>Kerolossafwat41@gmail.com</p></span>
+                        <span className="flex"><Earth className="mr-2" /><p>Egypt - Nasr City</p></span>
+                        <span className="flex"><Book className="mr-2" /><p>Computer Science</p></span>
+                    </div>
+
+                    <div className="w-full flex flex-col justify-center items-start mt-4">
+                        <h3 className="text-2xl ml-2 font-semibold">Skills</h3>
+                        <div className="w-full flex m-2 flex-wrap">
+                        <Badge variant="dark">Javascript</Badge>
+                        <Badge variant="dark">HTML</Badge>
+                        <Badge variant="dark">CSS</Badge>
+                        <Badge variant="dark">ReactJS</Badge>
+                        <Badge variant="dark">NextJS</Badge>
+                        <Badge variant="dark">MONGO</Badge>
+                        <Badge variant="dark">SQL</Badge>
+                        <Badge variant="dark">NODEJS</Badge>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="w-[calc(100%-500px)] mx-4 h-full bg-neutral-50 rounded-xl px-2 py-[25px] shadow-xl">
+                    <img src="/assets/res.png" className="w-full h-full object-contain"/>
+                </div>
+
+        </>
+    )
+}
+export default Profile;
--- src/pages/discover/components/SideBar.jsx
+++ src/pages/discover/components/SideBar.jsx
@@ -3,16 +3,21 @@ import { Button } from "@/components/ui/button";
 import { Book, BriefcaseBusiness, Calendar, Earth, IdCard, Mail, Scroll, Star, Video } from "lucide-react";
 import Tooltipcustom from "./Tooltipcustom";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import { Badge } from "@/components/ui/badge";
+import Profile from "./Profile";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+
 
 
 const SideBar = ()=>{
+    const [View,SetView] = useState("profile");
+    //profile , jobs , courses , papers , conference , favorites
     return(
         <>
         <div className="flex w-full h-[calc(100vh-100px)] ">
             <div className="bg-gray-50 w-[70px] flex flex-col justify-start items-center p-2 gap-y-1">
-                <Tooltipcustom message={"Your Info"}>
-                <Button variant={"light"} className="w-full h-[50px]">
+                <Tooltipcustom message={"Profile"}>
+                <Button onClick={()=>SetView("profile")} variant={"light"} className={"w-full h-[50px] "+ cn(View === "profile" && "bg-gray-800 text-white hover:bg-gray-600")}>
                 <Avatar>
                 <AvatarImage className="rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpNP5uxlf7Af8QWKuzNL1GjZPl712flND8jRCoTGgvB8sQdfdQ2qXrupv0yw0ggGcXBnE&usqp=CAU" />
                 <AvatarFallback></AvatarFallback>
@@ -21,68 +26,30 @@ const SideBar = ()=>{
                 </Tooltipcustom>
                 
                 <Tooltipcustom message={"Jobs"}>
-                <Button variant={"light"} className="w-full h-[50px]"><BriefcaseBusiness size={50} /></Button>
+                <Button onClick={()=>SetView("jobs")} variant={"light"} className={"w-full h-[50px] "+ cn(View === "jobs" && "bg-gray-800 text-white hover:bg-gray-600")}><BriefcaseBusiness /></Button>
                 </Tooltipcustom>
                 
                 <Tooltipcustom message={"Courses"}>
-                <Button variant={"light"} className="w-full h-[50px]"><Video size={100} /></Button>
+                <Button onClick={()=>SetView("courses")} variant={"light"} className={"w-full h-[50px] "+ cn(View === "courses" && "bg-gray-800 text-white hover:bg-gray-600")}><Video /></Button>
                 </Tooltipcustom>
 
                 <Tooltipcustom message={"Papers"}>
-                <Button variant={"light"} className="w-full h-[50px]"><Scroll size={36} /></Button>
+                <Button onClick={()=>SetView("papers")} variant={"light"} className={"w-full h-[50px] "+ cn(View === "papers" && "bg-gray-800 text-white hover:bg-gray-600")}><Scroll /></Button>
                 </Tooltipcustom>
 
                 <Tooltipcustom message={"Conference"}>
-                <Button variant={"light"} className="w-full h-[50px]"><Calendar /></Button>
+                <Button onClick={()=>SetView("conference")} variant={"light"} className={"w-full h-[50px] "+ cn(View === "conference" && "bg-gray-800 text-white hover:bg-gray-600")}><Calendar /></Button>
                 </Tooltipcustom>
 
                 <span className="w-full mt-auto">
-                <Tooltipcustom message={"Favorite"}>
-                <Button variant={"light"} className="w-full h-[50px]"><Star /></Button>
+                <Tooltipcustom message={"Favorites"}>
+                <Button onClick={()=>SetView("favorites")} variant={"light"} className={"w-full h-[50px] "+ cn(View === "favorites" && "bg-gray-800 text-white hover:bg-gray-600")}><Star /></Button>
                 </Tooltipcustom></span>
 
             </div>
             <div className="w-[calc(100%-70px)] flex p-4 bg-[url('/assets/download.svg')]">
-
-                <div className="flex flex-col justify-start items-center w-[500px] h-fit bg-neutral-50 rounded-xl px-2 py-[25px] shadow-xl">
-                    <div className="w-full flex justify-center items-center h-[40%]">
-                    <Avatar className="w-[200px] drop-shadow-xl">
-                    <AvatarImage className="rounded-full shadow-lg" src="https://avatars.githubusercontent.com/u/183719833?v=4" />
-                    <AvatarFallback><img className="rounded-full shadow-lg" src="/assets/user.webp" /></AvatarFallback>
-                    </Avatar>
-                    <div>
-                    <h2 className="ml-4 text-3xl font-bold font-sans">Kerolos Safwat</h2>
-                    <span className="ml-4 text-sm font-medium font-sans">Software Engineer</span>
-                    </div>
-                    </div>
-
-                    <div className="w-full flex flex-col p-2 gap-y-3 mt-4">
-                    <span className="flex"><IdCard className="mr-2" /><p className="font-bold">532016</p></span>
-                        <span className="flex"><Mail className="mr-2" /><p>Kerolossafwat41@gmail.com</p></span>
-                        <span className="flex"><Earth className="mr-2" /><p>Egypt - Nasr City</p></span>
-                        <span className="flex"><Book className="mr-2" /><p>Computer Science</p></span>
-                    </div>
-
-                    <div className="w-full flex flex-col justify-center items-start mt-4">
-                        <h3 className="text-2xl ml-2 font-semibold">Skills</h3>
-                        <div className="w-full flex m-2 flex-wrap">
-                        <Badge variant="dark">Javascript</Badge>
-                        <Badge variant="dark">HTML</Badge>
-                        <Badge variant="dark">CSS</Badge>
-                        <Badge variant="dark">ReactJS</Badge>
-                        <Badge variant="dark">NextJS</Badge>
-                        <Badge variant="dark">MONGO</Badge>
-                        <Badge variant="dark">SQL</Badge>
-                        <Badge variant="dark">NODEJS</Badge>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="w-[calc(100%-500px)] mx-4 h-full bg-neutral-50 rounded-xl px-2 py-[25px] shadow-xl">
-                    <img src="/assets/res.png" className="w-full h-full object-contain"/>
-                </div>
-
+            {View === "profile" && <Profile />}
+                
             </div>
         </div>
         </>
