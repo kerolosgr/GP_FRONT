0e857bd82de338b9dc335c12c07d01869c0960d3
kerolosgr <kerolossafwat44@gmail.com>
2025-03-14 00:58:21 +0200

    vite config allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    allowedHosts: [
+      'api.kerolos-safwat.me', // Add your host here
+      'localhost', // Allow localhost
+      '127.0.0.1', // Allow local IP
+    ],
     port: 3001,  // Set the port to 3001
   },
   plugins: [react(), tailwindcss()],
