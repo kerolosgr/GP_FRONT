a0f4f1164dcc288ed33231384059b268662c421e
kerolosgr <kerolossafwat44@gmail.com>
2025-03-14 00:51:09 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001,  // Set the port to 3001
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
