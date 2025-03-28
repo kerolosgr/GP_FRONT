46d7687c931d3a1e02ef4fb928e85e1e8fcf7121
kerolosgr <kerolossafwat44@gmail.com>
2025-03-29 00:11:50 +0200

    edit

--- src/pages/discover/components/Githubrepocard.jsx
+++ src/pages/discover/components/Githubrepocard.jsx
@@ -9,7 +9,7 @@ const Githubrepocard = ({repoName,repoLanguage,repoDate,repoUrl})=>{
             <div className="flex flex-col justify-start items-start w-[80%] h-full">
             <h4 className="text-[21px] font-semibold text-neutral-700">{repoName}</h4>
             <span className="flex items-center mt-2 gap-1.5">
-                <span className="block w-[10px] h-[10px] outline-2 outline-neutral-900 rounded-[50%]"></span>
+                <span className="block w-[10px] h-[10px] outline-2 outline-orange-500 rounded-[50%]"></span>
                 <p className="text-[12px] font-normal text-neutral-500">{repoLanguage}</p>
             </span>
             <p className="mt-auto text-[12px] font-normal text-neutral-500">updated on : {format(repoDate, "MMM dd yyyy")} , {formatDistance(subDays(repoDate, 3), new Date(), { addSuffix: true })}</p>
