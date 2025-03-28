f386e89d15eb00507eb9cb920fedb0b0d06725c4
kerolosgr <kerolossafwat44@gmail.com>
2025-03-29 00:45:17 +0200

    edit

--- src/pages/discover/components/Githubrepocard.jsx
+++ src/pages/discover/components/Githubrepocard.jsx
@@ -5,9 +5,9 @@ import { format,formatDistance,formatDate,subDays } from "date-fns"
 
 const Githubrepocard = ({repoName,repoLanguage,repoDate,repoUrl})=>{
     return(
-        <div className="w-full md:w-[450px] h-[150px] my-1 p-4 flex flex-col md:flex-row flex-shrink-0 justify-start items-start rounded-lg border border-gray-300">
+        <div className="w-full md:w-[450px] h-[170px] md:h-[150px] my-1 p-4 flex flex-col md:flex-row flex-shrink-0 justify-start items-start rounded-lg border border-gray-300">
             <div className="flex flex-col justify-start items-start w-[70%] h-full">
-            <h4 className="text-[21px] font-semibold text-neutral-700 line-clamp-2">{repoName}</h4>
+            <h4 className="text-[16px] md:text-[21px] font-semibold text-neutral-700 line-clamp-2">{repoName}</h4>
             <span className="flex items-center mt-2 gap-1.5">
                 <span className="block w-[10px] h-[10px] outline-2 outline-orange-500 rounded-[50%]"></span>
                 <p className="text-[12px] font-normal text-neutral-500">{repoLanguage}</p>
