bcaae59d55d05cf49f93b2cca3cbe336b72f0bf3
kerolosgr <kerolossafwat44@gmail.com>
2025-03-29 18:57:52 +0200

    edit

--- src/pages/discover/components/Githubrepocard.jsx
+++ src/pages/discover/components/Githubrepocard.jsx
@@ -2,15 +2,19 @@ import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import { Link } from "react-router-dom"
 import { format,formatDistance,formatDate,subDays } from "date-fns"
+import { languageColors } from "./GithubLanguageColor"
 
 const Githubrepocard = ({repoName,repoLanguage,repoDate,repoUrl})=>{
+    const languageColor = (language)=>{
+        return <span className={cn(`block w-[10px] h-[10px] outline-2 rounded-[50%]`)} style={{ outlineColor: languageColors[language] }}></span>
+    }
     return(
         <div className="w-full md:w-[450px] h-[170px] md:h-[150px] my-1 p-4 flex flex-col md:flex-row flex-shrink-0 justify-start items-start rounded-lg border border-gray-300">
-            <div className="flex flex-col justify-start items-start w-[70%] h-full">
+            <div className="flex flex-col justify-start items-start w-full md:w-[70%] h-full">
             <h4 className="text-[16px] md:text-[21px] font-semibold text-neutral-700 line-clamp-2">{repoName}</h4>
             <span className="flex items-center mt-2 gap-1.5">
-                <span className="block w-[10px] h-[10px] outline-2 outline-orange-500 rounded-[50%]"></span>
-                <p className="text-[12px] font-normal text-neutral-500">{repoLanguage}</p>
+                {languageColor(repoLanguage)}
+                <p className="text-[12px] font-normal text-neutral-500">{repoLanguage??"No Language"}</p>
             </span>
             <p className="mt-auto text-[12px] font-normal text-neutral-500">updated on {format(repoDate, "MMM dd yyyy")} , {formatDistance(subDays(repoDate, 3), new Date(), { addSuffix: true })}.</p>
             </div>
