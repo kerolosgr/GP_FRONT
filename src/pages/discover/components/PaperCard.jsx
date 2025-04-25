e50f1c5f0918a1848aae0836ef34d5bb06a82c9b
kerolosgr <kerolossafwat44@gmail.com>
2025-04-25 20:06:13 +0300

    paper edit

--- src/pages/discover/components/PaperCard.jsx
+++ src/pages/discover/components/PaperCard.jsx
@@ -1,7 +1,7 @@
 const PaperCard = ({title,author,date})=>{
     return(
         <>
-        <div className="flex flex-col justify-start items-start w-full md:w-[330px] min-h-[200px] p-4 rounded-xl border-2 hover:shadow transition duration-200 ease-in-out">
+        <div className="flex flex-col justify-start items-start w-full md:w-[320px] min-h-[200px] p-4 rounded-xl border-2 hover:shadow transition duration-200 ease-in-out">
             <h4 className="text-[16px] font-semibold text-gray-800 line-clamp-3">{title}</h4>
             <p className="text-gray-700 text-[14px] my-2">{author} <span className="font-semibold">{date}</span></p>
             <p className="text-gray-700 font-semibold text-[12px] line-clamp-2">We present a neural network structure, FramePack, to train next-frame (or next-frame-section) prediction models for video generation.</p>
