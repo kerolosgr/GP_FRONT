3a29e5b146db2a98865c2e21cc35eb4c43d58b51
kerolosgr <kerolossafwat44@gmail.com>
2025-04-25 20:10:42 +0300

    paper skeleton

--- src/pages/discover/components/PaperCardSkeleton.jsx
+++ src/pages/discover/components/PaperCardSkeleton.jsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const PaperCardSkeleton = ()=>{
     return(
-        <div className="flex flex-col justify-start items-start w-full md:w-[330px] min-h-[200px] p-4 rounded-xl border-2 hover:shadow transition duration-200 ease-in-out">
+        <div className="flex flex-col justify-start items-start w-full md:w-[320px] min-h-[200px] p-4 rounded-xl border-2 hover:shadow transition duration-200 ease-in-out">
             <Skeleton className="w-[100%] h-[18px] rounded"></Skeleton>
             <Skeleton className="w-[100%] h-[18px] rounded my-2"></Skeleton>
             <Skeleton className="w-[100%] h-[18px] rounded"></Skeleton>
