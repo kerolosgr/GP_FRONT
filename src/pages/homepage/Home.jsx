88a1e26a8dc3c5866f1333d99770651d705a66a8
kerolosgr <kerolossafwat44@gmail.com>
2025-05-01 22:26:09 +0300

    edit in homepage bg

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 const Home = ()=>{
  return(
     <>
-    <div className="flex w-full flex-col-reverse md:flex-row justify-between min-h-[calc(100vh-100px)] items-center bg-[url('/assets/download.svg')]">
+    <div className="flex w-full flex-col-reverse md:flex-row justify-between min-h-[calc(100vh-100px)] items-center bg-[radial-gradient(ellipse_at_top_left,_#e3e5e6,_#efe8c1,_#f3e7cb)]">
     <div className="md:ml-4 md:p-1 p-4 w-full md:w-[50%]">
     <h1 className="text-black font-bold text-[60px] md:text-[200px]">DevPal</h1>
     <h4 className="w-[85%] text-[21px]">Your ultimate companion in the developer journey. Find jobs, courses, tutorials, papers, and everything you need to level up your skills and career. DevPal: Empowering developers, one step at a time.
