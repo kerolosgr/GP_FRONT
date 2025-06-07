cded2defbaf666fb4231c8c05105c533885cf966
kerolosgr <kerolossafwat44@gmail.com>
2025-06-07 18:52:36 +0300

    ats score

--- src/pages/discover/components/AtsScore.jsx
+++ src/pages/discover/components/AtsScore.jsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import axios from "axios";
 import { Info, Loader2 } from "lucide-react";
 import { useState } from "react";
 import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
+import { toast } from "sonner";
 
 const AtsScore = ()=>{
     const [skills,setSkills] =useState('');
@@ -11,17 +13,38 @@ const AtsScore = ()=>{
     const [loading,setLoading]=useState(false);
     const [finish,setFinish] = useState(false);
     const [showDetails,setShowDetails]=useState(false);
+    const [recievedData,setRecievedData]=useState({});
+    const form = new FormData;
 
-    const getScore = ()=>{
+    const getScore = async()=>{
+        form.append("job_description",requirements)
         setLoading(true);
-        setTimeout(
-            ()=>{
-                setLoading(false);
-                setFinish(true);
-            },
-            1000
-        )
-    }
+        try{
+        const res = await axios.post('https://store-powerseller-bored-headline.trycloudflare.com/evaluate',form);
+        setRecievedData(res.data)
+        console.log(res.data);
+        setLoading(false);
+        setFinish(true);
+        }
+        catch(err){
+            setLoading(false);
+            toast('Sorry , Error Occured');
+        }
+        // setTimeout(
+        //     ()=>{
+        //         setLoading(false);
+        //         setFinish(true);
+        //     },
+        //     1000
+        // )
+
+    };
+
+    const onChangeResume = (e)=>{
+        const file = e.target.files[0];
+        form.append("cv_file",file);
+        console.log(file)
+    };
 
 
     return(
@@ -32,10 +55,10 @@ const AtsScore = ()=>{
                 {finish?
                 <>
                 <div className="w-full h-fit flex justify-center items-center my-4">
-                <CircularProgressbar className='w-[140px] h-[140px] aspect-square my-2' minValue={0} maxValue={100} value={80} text={`${80}%`} styles={buildStyles({rotation: 0.5 , pathColor: `rgba(255, 216, 96, 1`,textColor: '#706f6e',trailColor: 'transparent',pathTransitionDuration: 0.5,})} />
+                <CircularProgressbar className='w-[140px] h-[140px] aspect-square my-2' minValue={0} maxValue={100} value={Math.ceil(recievedData.ats_score)} text={`${Math.ceil(recievedData.ats_score)}%`} styles={buildStyles({rotation: 0.5 , pathColor: `rgba(255, 216, 96, 1`,textColor: '#706f6e',trailColor: 'transparent',pathTransitionDuration: 0.5,})} />
                 </div>
                 <div className="flex items-center justify-center w-full">
-                <p className="text-black text-[14px] text-center px-4">Your Resume Scored 80%</p>
+                <p className="text-black text-[14px] text-center px-4">Your Resume Scored {(recievedData.ats_score).ceil}%</p>
                 <Button onClick={()=>setShowDetails(true)} className="rounded-full"><Info/></Button>
                 </div>
                 </>
@@ -48,14 +71,22 @@ const AtsScore = ()=>{
                 :
                 <>
                 <Input onChange={(e)=>{setRequirements(e.target.value)}} placeholder="Job Requirements" className="bg-white mt-2 w-full"/>
-                <Input onChange={(e)=>{setSkills(e.target.value)}} placeholder="Your Skills" className="bg-white my-1 w-full"/>
+                {/* <Input onChange={(e)=>{setSkills(e.target.value)}} placeholder="Your Skills" className="bg-white my-1 w-full"/> */}
+                <Input onChange={onChangeResume} name="cv_file" type="file" className="my-2 bg-white"/>
                 <Button onClick={getScore} className="ml-auto my-2">Calculte</Button>
                 </>
                 }
                 </div>
                 <Dialog open={showDetails} onOpenChange={setShowDetails}>
-                    <DialogContent>
+                    <DialogContent className="min-w-[70%] p-[50px]">
                     <p>ATS Score Details</p>
+                    <p>Your ATS Score : {Math.ceil(recievedData.ats_score)}</p>
+                    <p>Education Match : {recievedData.education_match}</p>
+                    <p>Experience Match : {recievedData.experience_match}</p>
+                    <p className="font-semibold" >FeedBack : {recievedData.feedback}</p>
+                    <p>Resume Entities : {recievedData?.resume_entities?.skills?.join(' , ')}</p>
+                    <p>Missing Skills : {recievedData?.missing_skills?.length < 1 ? "None" : recievedData?.missing_skills?.join(' - ') }</p>
+                    <p>Similarity : {Math.ceil(recievedData.semantic_similarity)}</p>
                     </DialogContent>
                 </Dialog>
         </>
